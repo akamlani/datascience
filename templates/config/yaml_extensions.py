6c113710a794269a08d38f4ffe54d43a5fdeeea9
arikamlani <ari.kamlani@edu.dsti.institute>
2015-12-27 15:04:07 +0100

    clean templates

--- templates/config/yaml_extensions.py
+++
@@ -1,6 +0,0 @@
-import yaml
-
-def load_config(file):
-	'''file as *.yml extension'''
-	credentials = yaml.load(open(file))
-	return credentials
--- templates/utilities/collections_extensions.py
+++
@@ -1,8 +0,0 @@
-from collections import defaultdict
-
-
-def create_sorted_dict():
-	d = defaultdict(int)
-	keys = d.keys()
-	keys = sorted(keys, key=lambda s: s.lower())
-	for k in keys: v = d[k]; print "%s: %d" % (k,v)
